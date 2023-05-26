878f98a25aa5f77f08f84fd49fa8957d443adf09
zulhusnihariz <zulhusnihariz97@gmail.com>
2023-05-26 14:48:53 +0800

    cleanup template files

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
 import './App.css';
-import { Routes, Route } from "react-router-dom";
-import MainExplorer from './pages/Explorer/MainExplorer'
+import { Routes, Route } from 'react-router-dom';
+import MainExplorer from './pages/Explorer/MainExplorer';
 import NavbarLayout from './components/Layout/NavbarLayout';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="w-full h-screen flex flex-col justify-start ">
-        <Routes>
-          <Route path="/" element={<NavbarLayout />}>
-            <Route index element={<MainExplorer />} />
-          </Route>
-        </Routes>
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="w-full h-screen flex flex-col justify-start ">
+				<Routes>
+					<Route path="/" element={<NavbarLayout />}>
+						<Route index element={<MainExplorer />} />
+					</Route>
+				</Routes>
+			</div>
+		</div>
+	);
 }
 
 export default App;
