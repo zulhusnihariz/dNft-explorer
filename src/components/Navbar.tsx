e9dd813026551a1c5b7b7a3b825df4549b4f4093
zulhusnihariz <zulhusnihariz97@gmail.com>
2023-06-09 21:49:08 +0800

    update Navbar - add navigation to TransactionExplorer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,35 @@
 import { useEffect } from 'react';
 import logo from '../assets/lineagelogo.png';
+import Dropdown from 'rc-dropdown';
+import '../assets/rc-dropdown.css';
+import Menu, { MenuItem } from 'rc-menu';
+import { NavLink } from 'react-router-dom';
+import { ArrowIcon } from '../assets/icons';
+
+const CustomNavLink = ({
+	children,
+	to,
+	className,
+}: {
+	children: React.ReactNode;
+	to: string;
+	className: string;
+}) => {
+	return (
+		<NavLink
+			to={to}
+			className={className}
+			style={({ isActive }) => {
+				return {
+					color: isActive ? '#0784c3' : '',
+				};
+			}}
+		>
+			{children}
+		</NavLink>
+	);
+};
+
 const Navbar = () => {
 	// get the sticky element
 	useEffect(() => {
@@ -13,15 +43,36 @@ const Navbar = () => {
 		observer.observe(stickyElm as HTMLElement);
 	});
 
+	const menuItems = [
+		<MenuItem
+			key="txs"
+			className="border-t-4 border-[#6E7DD6] hover:bg-[#E9ECEF] py-1"
+			onClick={() => console.log('tx clicked')}
+		>
+			<CustomNavLink to="/txs" className="px-8 active:text-red">
+				Transactions
+			</CustomNavLink>
+		</MenuItem>,
+	];
+
 	return (
-		<header aria-label="SEED Header pt-2">
+		<header aria-label="SEED Header pt-2 h-20">
 			<div className=" mx-auto max-w-screen-xl flex h-20 items-center justify-between">
-				<a className="block " href="/">
+				<CustomNavLink className="block" to="/">
 					<img src={logo} alt="" width={350} height={41} className="" />
-				</a>
+				</CustomNavLink>
 				<div className="flex gap-4 font-semibold text-sm ">
-					<h4 className="cursor-pointer">HOME</h4>
-					<h4 className="cursor-pointer">MIGRATE TO LINEAGE</h4>
+					<CustomNavLink className="p-1 " to="/">
+						HOME
+					</CustomNavLink>
+					<h4 className="p-1 cursor-pointer">
+						<Dropdown overlay={<Menu>{menuItems}</Menu>}>
+							<button className="flex">
+								NFTs <ArrowIcon />
+							</button>
+						</Dropdown>
+					</h4>
+					<h4 className="p-1 cursor-pointer">MIGRATE TO LINEAGE</h4>
 				</div>
 			</div>
 		</header>
